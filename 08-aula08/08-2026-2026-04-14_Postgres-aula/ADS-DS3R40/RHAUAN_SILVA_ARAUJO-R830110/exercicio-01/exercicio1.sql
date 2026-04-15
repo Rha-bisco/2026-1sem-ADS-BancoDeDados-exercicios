aa6f6034d2b5097ca5f2bcad1a4aa2c496803de2
Rha-bisco <rhauan.s.araujo@gmail.com>
2026-04-14 21:44:53 -0300

    Add SQL query to extract data into relatorio02

--- 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS3R40/RHAUAN_SILVA_ARAUJO-R830110/exercicio-01/exercicio1.sql
+++ 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS3R40/RHAUAN_SILVA_ARAUJO-R830110/exercicio-01/exercicio1.sql
@@ -1,1 +1,15 @@
 //exercicio1
+SELECT 
+    cnpj, 
+    status, 
+    nome, 
+    data_atividade, 
+    estado
+    into
+     rhauan_silva_araujo_r830110.relatorio02
+FROM 
+    public.cnpj_brasil
+WHERE 
+    estado = 'AC' AND
+     CAST(data_atividade AS DATE) > '2024-01-01' limit 2000;
+
